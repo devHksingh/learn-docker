3651b04bbb02a3a645fe1923a07f45d7f68681eb
devHksingh <hksingh.dev@gmail.com>
2024-11-26 11:57:17 +0530

    change node version

--- 01_layer_optimise/src/index.ts
+++ 01_layer_optimise/src/index.ts
@@ -1,13 +1,13 @@
 
 import { PrismaClient } from "@prisma/client";
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
 
 const client = new PrismaClient();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
     res.json({
         message: "Healthy server"
     })
